c91f03cf66b0584e4471d224be085286858037cd
Dorian Marié <dorian@dorianmarie.com>
2025-01-04 09:28:30 +0100

    fix hosts on kamal

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,10 @@ class Code
     config.active_support.to_time_preserves_timezone = :zone
     config.autoload_lib(ignore: %w[assets tasks])
     config.exceptions_app = routes
-    config.hosts += ENV.fetch("HOSTS").split(",")
     config.mission_control.jobs.http_basic_auth_enabled = false
     config.mission_control.jobs.show_console_help = false
     config.session_store :cookie_store, expire_after: 1.year
+    config.hosts += ENV.fetch("HOSTS").split(",")
+    config.hosts.clear
   end
 end
