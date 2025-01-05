a627de88f91562d6737e5c2ea605ed4d84f99960
Dorian Marié <dorian@dorianmarie.com>
2025-01-05 22:06:32 +0100

    try to fix emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "dorian@codedorian.com"
+  default from: "dorian@dorianmarie.com"
   layout "mailer"
 end
