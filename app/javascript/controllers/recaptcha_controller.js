a4a0681301b33cb259797fd83a63b252a370bdb1
Dorian Marié <dorian@dorianmarie.com>
2025-03-24 18:32:21 +0100

    improve recaptcha script loading and execution logic, remove compact_blank from notification data path

--- app/javascript/controllers/recaptcha_controller.js
+++ app/javascript/controllers/recaptcha_controller.js
@@ -1,38 +1,50 @@
 import { Controller } from "@hotwired/stimulus";
 
+const INTERVAL_MILLISECONDS = 5000;
+
 export default class extends Controller {
   static targets = ["response", "action"];
 
   connect() {
     this._input().disabled = true;
 
-    const script = document.createElement("script");
-    script.async = true;
-    script.defer = true;
-    script.onload = this.load.bind(this);
-    script.src = `
-      https://www.google.com/recaptcha/api.js
-        ?render=${window.RECAPTCHA_SITE_KEY}
-        &time=${Date.now()}
-        &random=${Math.random()}
-    `.replace(/\s/g, "");
+    if (!window.grecaptcha) {
+      const script = document.createElement("script");
+      script.async = true;
+      script.defer = true;
+      script.onload = this.load.bind(this);
+      script.src = `
+        https://www.google.com/recaptcha/api.js
+          ?render=${window.RECAPTCHA_SITE_KEY}
+          &time=${Date.now()}
+          &random=${Math.random()}
+      `.replace(/\s/g, "");
 
-    document.head.appendChild(script);
+      document.head.appendChild(script);
+    }
+
+    this.interval = setInterval(this.execute.bind(this), INTERVAL_MILLISECONDS);
+  }
+
+  disconnect() {
+    clearInterval(this.interval);
   }
 
   load() {
-    grecaptcha.ready(() => {
+    window.grecaptcha.ready(() => {
       this.execute();
     });
   }
 
   execute() {
-    grecaptcha
-      .execute(window.RECAPTCHA_SITE_KEY, { action: this.actionTarget.value })
-      .then((token) => {
-        this.responseTarget.value = token;
-        this._input().disabled = false;
-      });
+    if (window.grecaptcha) {
+      grecaptcha
+        .execute(window.RECAPTCHA_SITE_KEY, { action: this.actionTarget.value })
+        .then((token) => {
+          this.responseTarget.value = token;
+          this._input().disabled = false;
+        });
+    }
   }
 
   _input() {
