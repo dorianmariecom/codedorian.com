5ca466383243a68c6bff79f08901cd0e97b2c81b
Dorian Marié <dorian@dorianmarie.com>
2025-03-25 22:08:18 +0100

    pre-commit hook bin/ci

---
+++ config/ci.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+CI.run do
+  step "rubocop", "bin/rubocop"
+  step "brakeman", "bin/brakeman", "-A", "-q", "--color"
+  step "importmap outdated", "bin/importmap", "outdated"
+  step "importmap audit", "bin/importmap", "audit"
+  step "bundler audit", "bin/bundler-audit", "check", "--update"
+  step "npm audit", "npm", "audit"
+  step "rspec", "bin/rspec"
+end
