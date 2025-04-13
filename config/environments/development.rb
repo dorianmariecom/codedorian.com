1f88c97cc13e1b2f32f8cb09b2dbd6e037dca4b8
Dorian Marié <dorian@dorianmarie.com>
2025-04-13 10:05:22 +0000

    clean up code formatting and fix duplicate entries in locale file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,6 +25,5 @@ Rails.application.configure do
   config.enable_reloading = true
   config.i18n.raise_on_missing_translations = true
   config.server_timing = true
-  config.action_mailer.smtp_settings =
-    Config.smtp_settings
+  config.action_mailer.smtp_settings = Config.smtp_settings
 end
