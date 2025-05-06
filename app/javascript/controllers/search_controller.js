eb13c57027264b65d56793142b4cbba5768b26b7
Dorian Marié <dorian@dorianmarie.com>
2025-05-06 14:45:22 +0200

    progress, names and ids for root

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -4,10 +4,8 @@ export default class extends Controller {
   static targets = [
     "advanced",
     "itemTemplate",
-    "buttonTemplate",
+    "emptyTemplate",
   ];
-
-  static classes = ["group"];
 
   static values = {
     defaultType: { type: String, default: "key_value" },
@@ -15,9 +13,20 @@ export default class extends Controller {
   };
 
   addItem(event) {
-    const group = event.target.closest(".js-search-group");
-    const newItem = this.itemTemplateTarget.content.cloneNode(true);
-    group.appendChild(newItem);
+    const empty = event.target.closest(".js-search-empty");
+    empty.remove()
+
+    const html = Object.entries(this._rootValues()).reduce((html, [key, value]) => {
+      return html.replaceAll(`{${key}}`, this._escapeHtml(value))
+    }, this.itemTemplateTarget.innerHTML);
+
+    this.advancedTarget.insertAdjacentHTML('beforeend', html);
+  }
+
+  orItem(event) {
+  }
+
+  andItem(event) {
   }
 
   chooseKeyValue(event) {
@@ -49,6 +58,37 @@ export default class extends Controller {
   advancedOpenValueChanged(advancedOpen) {
     if (advancedOpen !== this.advancedTarget.open) {
       this.advancedTarget.open = advancedOpen;
+    }
+  }
+
+  _escapeHtml(unsafe) {
+    return (unsafe ?? "")
+      .toString()
+      .replaceAll("&", "&amp;")
+      .replaceAll("<", "&lt;")
+      .replaceAll(">", "&gt;")
+      .replaceAll('"', "&quot;")
+      .replaceAll("'", "&#039;");
+  }
+
+  _rootValues() {
+    return {
+      type_id: "search_type",
+      type_name: "search[type]",
+      free_field_name: "search[free_field]",
+      free_field_id: "search_free_field",
+      key_value_key_name: "search[key_value][key]",
+      key_value_key_id: "search_key_value_key",
+      key_value_operator_name: "search[key_value][operator]",
+      key_value_operator_id: "search_key_value_operator",
+      key_value_type_name: "search[key_value][type]",
+      key_value_type_id: "search_key_value_type",
+      key_value_value_name: "search[key_value][value]",
+      key_value_value_id: "search_key_value_value",
+      key_value_first_name: "search[key_value][first]",
+      key_value_first_id: "search_key_value_first",
+      key_value_last_name: "search[key_value][last]",
+      key_value_last_id: "search_key_value_last",
     }
   }
 }
