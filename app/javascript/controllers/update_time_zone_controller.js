e8fb44ed06024314ea5f2830ac52146c5a24616e
Dorian Marié <dorian@dorianmarie.com>
2025-06-29 08:53:21 +0200

    prevent flash messages from disappearing

--- app/javascript/controllers/update_time_zone_controller.js
+++ app/javascript/controllers/update_time_zone_controller.js
@@ -18,9 +18,5 @@ export default class extends Controller {
       },
       body: JSON.stringify({ time_zone: timeZone }),
     });
-
-    if (response.ok) {
-      window.location.reload();
-    }
   }
 }
