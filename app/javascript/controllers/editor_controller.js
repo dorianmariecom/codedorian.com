9a4459cbb2b1b25f1379e04293c4b5a131f4b6b5
Dorian Marié <dorian@dorianmarie.com>
2025-07-26 11:13:30 +0200

    basic editor

--- app/javascript/controllers/editor_controller.js
+++ app/javascript/controllers/editor_controller.js
@@ -1,5 +1,15 @@
 import { Controller } from "@hotwired/stimulus";
 import { EditorView } from "codemirror";
+import {
+  EditorView,
+  keymap,
+  drawSelection,
+  rectangularSelection,
+  crosshairCursor,
+  lineNumbers,
+} from "@codemirror/view";
+import { indentOnInput } from "@codemirror/language";
+import { history, historyKeymap } from "@codemirror/commands";
 
 export default class extends Controller {
   static targets = ["input", "editor"];
@@ -8,6 +18,20 @@ export default class extends Controller {
     this.editor = new EditorView({
       doc: this.inputTarget.value,
       parent: this.editorTarget,
+      extensions: [
+        EditorView.lineWrapping,
+        EditorView.updateListener.of((update) => {
+          this.inputTarget.value = update.state.doc.toString();
+        }),
+        keymap.of(historyKeymap),
+        bracketMatching(),
+        crosshairCursor(),
+        drawSelection(),
+        history(),
+        indentOnInput(),
+        lineNumbers(),
+        rectangularSelection(),
+      ],
     });
   }
 
