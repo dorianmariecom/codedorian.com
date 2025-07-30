57fd700f834498fa44e293a830defa1504c70e83
Dorian Marié <dorian@dorianmarie.com>
2025-07-30 18:43:24 +0200

    fix js

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,36 +1,37 @@
 # frozen_string_literal: true
 
 pin "application", integrity: true
-pin "i18n", integrity: true
-pin "debounce", integrity: true
 pin "constants", integrity: true
-pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@8.0.16/app/javascript/turbo/index.js", integrity: "sha384-v7FP4RPRQwLyHEIKPJFJ+sv3t0A31DdzGe25ym5ivgGf0YRQOe4KoWi+jfhQ7gXv"
-pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js", integrity: "sha384-NStGXzUH4pFNGrLRkzXYz7zzDn8S6MvFPwfKSuYf6TlX25BrNVP4oCN2S04XUuOI"
+pin "consumer", integrity: true
+pin "debounce", integrity: true
+pin "i18n", integrity: true
+pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@8.0.16/app/javascript/turbo/index.js", integrity: true
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js", integrity: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", integrity: true
 pin_all_from "app/javascript/controllers", under: "controllers", integrity: true
-pin "intl-tel-input", to: "https://ga.jspm.io/npm:intl-tel-input@25.3.1/build/js/intlTelInput.js", integrity: "sha384-J+0iqBG/pW6z72+cw/Jc9mvPmIiqZ5irkFmjz2AU1DewOfdBdJfCCEV2Gc0n7kjZ"
-pin "intl-tel-input/build/js/utils.js", to: "https://ga.jspm.io/npm:intl-tel-input@25.3.1/build/js/utils.js", integrity: "sha384-J3nfJG18IEq/SMfDUZ/wTNcUJqts5ShiAOHVFfaYKSVr6JzwtYJ1aDEnmJ3Rzuuk"
-pin "stimulus-places-autocomplete", to: "https://ga.jspm.io/npm:stimulus-places-autocomplete@0.5.0/dist/stimulus-places-autocomplete.es.js", integrity: "sha384-eygLlFILdx93ZU7elE8LWHEndzRSkEBJmdSDf/j46QewD/2/UaAhM49HjuzqzXVO"
-pin "@googlemaps/js-api-loader", to: "@googlemaps--js-api-loader.js", integrity: "sha384-M/X+fp2wzaPOnHBnx2aochjpe5Y5Ye/7OEoh7Kf3Yo66yyg/+hf4Sv6S8wtfBhq7" # @1.16.10
-pin "@hotwired/turbo", to: "https://ga.jspm.io/npm:@hotwired/turbo@8.0.13/dist/turbo.es2017-esm.js", integrity: "sha384-Ar5BI7e7pT8hROa/8lS1X1ANGto4Pa7UJb06l16NbEqGKD/CCab3ZTrtWi18TVam"
-pin "local-time", to: "https://ga.jspm.io/npm:local-time@3.0.3/app/assets/javascripts/local-time.es2017-esm.js", integrity: "sha384-O5lr+l3FlQwSPyPZjAMu1oF63IQc2sIhmTO/okDLD275BuGpTCtreckeYgW19nWJ"
-pin "trix", to: "https://ga.jspm.io/npm:trix@2.1.15/dist/trix.esm.min.js", integrity: "sha384-JzDocgVhAcvmrjr+DwV77W39Kw71iPkY8niULmFTUk9Vn2P9RE3Mp4jxDzmSqBk/"
-pin "@rails/actiontext", to: "https://ga.jspm.io/npm:@rails/actiontext@8.0.200/app/assets/javascripts/actiontext.esm.js", integrity: "sha384-+9Cxa7T6jHB1IDIHL7VaJltmJmAlb+LhYWEVOuj7EhlKjiDriNqW5rQ0rHMskNZ4"
-pin "@hotwired/hotwire-native-bridge", to: "https://ga.jspm.io/npm:@hotwired/hotwire-native-bridge@1.2.1/dist/hotwire-native-bridge.js", integrity: "sha384-j3LbbmrKYx3I7In5CvphoO79dByaL3G68IDmJ7PizDomfnr9RbTSu6EIOoIndZJV"
-pin "thememirror", to: "https://ga.jspm.io/npm:thememirror@2.0.1/dist/index.js", integrity: "sha384-03W9htrOxsYlKyBiFoZuvdjjb84dLtw0nKHA3stt21lDchUtYad6wrXRgG9DkaUv"
-pin "@codemirror/language", to: "https://ga.jspm.io/npm:@codemirror/language@6.11.2/dist/index.js", integrity: "sha384-UsGgFKKkOD4n9/4u1kz/WxP3aN5SUrhMOYsVhhMNnZMjq7hn9DFGzBTYgzTwsp5p"
-pin "@codemirror/state", to: "https://ga.jspm.io/npm:@codemirror/state@6.5.2/dist/index.js", integrity: "sha384-ipm8CvCKvH4gtoFjClbsFP88zhwDi1zFU2xVh8Wc/Eta6jLmfMR6JAGJhRD9gnG+"
-pin "@codemirror/view", to: "https://ga.jspm.io/npm:@codemirror/view@6.38.1/dist/index.js", integrity: "sha384-rvdgw/rLU5o0h4idWkGa3Bb9ho0udf+U0SyKciNDV/TFslvOinJmseE48Yxfx6Px"
-pin "@lezer/common", to: "https://ga.jspm.io/npm:@lezer/common@1.2.3/dist/index.js", integrity: "sha384-PwiGiKJWQUBt1l6B+fcSBL3AwBW31pC0fpuT9AU4fDeU3sTuqYdGXwId8WZbf7/+"
-pin "@lezer/highlight", to: "https://ga.jspm.io/npm:@lezer/highlight@1.2.1/dist/index.js", integrity: "sha384-qdccaKiuDuYFzuLSTSMLOVB1514htr0rI0fHBhZ6nfKdcaUnFapFfBjWxFbtEsUK"
-pin "@marijn/find-cluster-break", to: "https://ga.jspm.io/npm:@marijn/find-cluster-break@1.0.2/src/index.js", integrity: "sha384-kLSM9S1+J/ib5Zdf4SzA6rHld1NKrRiG7f8aWshPx6AAqVbLNohoechqQsXYOJzT"
-pin "crelt", to: "https://ga.jspm.io/npm:crelt@1.0.6/index.js", integrity: "sha384-qZ83Z6LSUkjCuDQyHiXk6So7Gy0jhGvdPVF+Gaym89QtnU4labLEvJGjFULdaiBH"
-pin "style-mod", to: "https://ga.jspm.io/npm:style-mod@4.1.2/src/style-mod.js", integrity: "sha384-WAoEmNrYaaF9MTkCjcpipYtTVbvPnfvABr+Vk6TpkIh22+Om77VahyMxU7ngyfRw"
-pin "w3c-keyname", to: "https://ga.jspm.io/npm:w3c-keyname@2.2.8/index.js", integrity: "sha384-uvMzZDawhUugZaMAVtrY8NRCR0UzEYeUSayncokEovR02ZH1YtwOOPXkD6SU33rL"
-pin "codemirror", to: "https://ga.jspm.io/npm:codemirror@6.0.2/dist/index.js", integrity: "sha384-+a4iWLEyBBbSd1Ns5O54INpL/m+YTct+nSt2cjQ2YTLm0E4DBoXSMx5gFnDTM9AZ"
-pin "@codemirror/autocomplete", to: "https://ga.jspm.io/npm:@codemirror/autocomplete@6.18.6/dist/index.js", integrity: "sha384-Sg/G5qkwgck/HSmGzFZqIv0QIDkIgTSc1Kpu88SVvgfHPMkUlUnuNcWgcO959zVd"
-pin "@codemirror/commands", to: "https://ga.jspm.io/npm:@codemirror/commands@6.8.1/dist/index.js", integrity: "sha384-gwZYXnTVqQrG7PoF6Q5WN44XTh3M8JmkULiopQg3l6SVCTPTftqquHyY52KL3dUG"
-pin "@codemirror/lint", to: "https://ga.jspm.io/npm:@codemirror/lint@6.8.5/dist/index.js", integrity: "sha384-c+6Wsn6Nau/IfF/Icz3TAMFRoji9bFpZUryvQ6Fz4/ynRUnZfAXF+ezpPeygDlkA"
-pin "@codemirror/search", to: "https://ga.jspm.io/npm:@codemirror/search@6.5.11/dist/index.js", integrity: "sha384-nDJNDdYyrcheAaFhLyip1674PVidOqrpN5HQORVjgIBXq+tougjQqLO5fLUO6NDE"
-pin "@rails/actioncable", to: "https://ga.jspm.io/npm:@rails/actioncable@8.0.200/app/assets/javascripts/actioncable.esm.js", integrity: "sha384-J9kCXP+j3uFXQw6/pfAdLmqYNZ019ggd096Lebw+1crESHJvLM3wRMOF+il4u0Gp"
-pin "@rails/actioncable/src", to: "https://ga.jspm.io/npm:@rails/actioncable@8.0.200/src/index.js", integrity: "sha384-oGoEWlBEWjlcDjT5Z8LqSP9tQQ/9dGM6dwAD11vwM5QKnIb2rSxef18v6MUS0BUR"
+pin "intl-tel-input", to: "https://ga.jspm.io/npm:intl-tel-input@25.3.1/build/js/intlTelInput.js", integrity: true
+pin "intl-tel-input/build/js/utils.js", to: "https://ga.jspm.io/npm:intl-tel-input@25.3.1/build/js/utils.js", integrity: true
+pin "stimulus-places-autocomplete", to: "https://ga.jspm.io/npm:stimulus-places-autocomplete@0.5.0/dist/stimulus-places-autocomplete.es.js", integrity: true
+pin "@googlemaps/js-api-loader", to: "@googlemaps--js-api-loader.js", integrity: true # @1.16.10
+pin "@hotwired/turbo", to: "https://ga.jspm.io/npm:@hotwired/turbo@8.0.13/dist/turbo.es2017-esm.js", integrity: true
+pin "local-time", to: "https://ga.jspm.io/npm:local-time@3.0.3/app/assets/javascripts/local-time.es2017-esm.js", integrity: true
+pin "trix", to: "https://ga.jspm.io/npm:trix@2.1.15/dist/trix.esm.min.js", integrity: true
+pin "@rails/actiontext", to: "https://ga.jspm.io/npm:@rails/actiontext@8.0.200/app/assets/javascripts/actiontext.esm.js", integrity: true
+pin "@hotwired/hotwire-native-bridge", to: "https://ga.jspm.io/npm:@hotwired/hotwire-native-bridge@1.2.1/dist/hotwire-native-bridge.js", integrity: true
+pin "thememirror", to: "https://ga.jspm.io/npm:thememirror@2.0.1/dist/index.js", integrity: true
+pin "@codemirror/language", to: "https://ga.jspm.io/npm:@codemirror/language@6.11.2/dist/index.js", integrity: true
+pin "@codemirror/state", to: "https://ga.jspm.io/npm:@codemirror/state@6.5.2/dist/index.js", integrity: true
+pin "@codemirror/view", to: "https://ga.jspm.io/npm:@codemirror/view@6.38.1/dist/index.js", integrity: true
+pin "@lezer/common", to: "https://ga.jspm.io/npm:@lezer/common@1.2.3/dist/index.js", integrity: true
+pin "@lezer/highlight", to: "https://ga.jspm.io/npm:@lezer/highlight@1.2.1/dist/index.js", integrity: true
+pin "@marijn/find-cluster-break", to: "https://ga.jspm.io/npm:@marijn/find-cluster-break@1.0.2/src/index.js", integrity: true
+pin "crelt", to: "https://ga.jspm.io/npm:crelt@1.0.6/index.js", integrity: true
+pin "style-mod", to: "https://ga.jspm.io/npm:style-mod@4.1.2/src/style-mod.js", integrity: true
+pin "w3c-keyname", to: "https://ga.jspm.io/npm:w3c-keyname@2.2.8/index.js", integrity: true
+pin "codemirror", to: "https://ga.jspm.io/npm:codemirror@6.0.2/dist/index.js", integrity: true
+pin "@codemirror/autocomplete", to: "https://ga.jspm.io/npm:@codemirror/autocomplete@6.18.6/dist/index.js", integrity: true
+pin "@codemirror/commands", to: "https://ga.jspm.io/npm:@codemirror/commands@6.8.1/dist/index.js", integrity: true
+pin "@codemirror/lint", to: "https://ga.jspm.io/npm:@codemirror/lint@6.8.5/dist/index.js", integrity: true
+pin "@codemirror/search", to: "https://ga.jspm.io/npm:@codemirror/search@6.5.11/dist/index.js", integrity: true
+pin "@rails/actioncable", to: "https://ga.jspm.io/npm:@rails/actioncable@8.0.200/app/assets/javascripts/actioncable.esm.js", integrity: true
+pin "@rails/actioncable/src", to: "https://ga.jspm.io/npm:@rails/actioncable@8.0.200/src/index.js", integrity: true
