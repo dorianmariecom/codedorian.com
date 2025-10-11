2c11e5a4490484f17c40871d47f40fa3af368f15
Dorian Marié <dorian@dorianmarie.com>
2025-10-11 20:03:59 +0200

    fix program evaluation

--- app/models/concerns/perform_later_concern.rb
+++ app/models/concerns/perform_later_concern.rb
@@ -2,7 +2,7 @@
 
 module PerformLaterConcern
   def perform_later(clazz, wait_until: nil, arguments: nil, context: nil)
-    job = clazz.set(wait_until: wait_until).perform_later(arguments)
+    job = clazz.set(wait_until: wait_until).perform_later(**arguments)
     JobContext.create!(active_job_id: job.job_id, context: context)
   end
 end
