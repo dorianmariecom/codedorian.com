e13970264af7dac23be336e32cea77c93ae17fd1
Dorian Marié <dorian@dorianmarie.com>
2025-10-29 21:12:56 +0100

    remove missing control jobs

--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,6 @@ module CodeApp
     config.active_support.to_time_preserves_timezone = :zone
     config.autoload_lib(ignore: %w[assets tasks])
     config.exceptions_app = routes
-    config.mission_control.jobs.http_basic_auth_enabled = false
-    config.mission_control.jobs.show_console_help = false
     config.session_store :cookie_store, expire_after: 1.year
     config.hosts += ENV.fetch("HOSTS").split(",")
     config.hosts += %w[::1 127.0.0.1 localhost]
