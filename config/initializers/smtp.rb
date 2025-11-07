04ad2ead87750cdedee43ad79e4e95bcdcc39025
Dorian Marié <dorian@dorianmarie.com>
2025-11-07 19:56:59 +0100

    fix deploy

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
 ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = Config.smtp_settings
+ActionMailer::Base.smtp_settings = Config.smtp_settings unless ENV["DUMMY"]
