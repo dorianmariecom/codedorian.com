28f36de5cd52d56f6ae0231a9ad8fa93e390dd56
Dorian Marié <dorian@dorianmarie.com>
2025-11-09 17:34:36 +0100

    limits concurrency

--- app/jobs/scheduling_program_job.rb
+++ app/jobs/scheduling_program_job.rb
@@ -3,12 +3,12 @@
 class SchedulingProgramJob < ApplicationJob
   queue_as(:default)
 
+  limits_concurrency(to: 1, key: ->(program:) { program }, duration: 5.minutes)
+
   def perform(program:)
     Current.with(user: program.user, program: program) do
-      Program.with_advisory_lock("SchedulingProgramJob:#{program.id}") do
-        if program.scheduled_now?
-          ProgramEvaluateJob.perform_later(program: program)
-        end
+      if program.scheduled_now?
+        ProgramEvaluateJob.perform_later(program: program)
       end
     end
   end
