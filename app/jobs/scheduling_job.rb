5aa621698dd36b409aae7ae215fe8d2abfc256b1
Dorian Marié <dorian@dorianmarie.com>
2025-11-17 21:05:20 +0100

    more threads for scheduling and default queues

--- app/jobs/scheduling_job.rb
+++ app/jobs/scheduling_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SchedulingJob < ContextJob
-  queue_as(:default)
+  queue_as(:scheduling)
 
   limits_concurrency(key: "SchedulingJob", on_conflict: :discard)
 
--- app/jobs/scheduling_program_job.rb
+++ app/jobs/scheduling_program_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SchedulingProgramJob < ContextJob
-  queue_as(:default)
+  queue_as(:scheduling)
 
   limits_concurrency(key: ->(program:, **) { program }, on_conflict: :discard)
 
