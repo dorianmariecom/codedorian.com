4e93e53b005e21aae1cfb44137d51b6ace014d6b
Dorian Marié <dorian@dorianmarie.com>
2025-12-09 21:49:55 +0100

    clean job contexts when job finishes

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -6,7 +6,13 @@ class ApplicationJob < ActiveJob::Base
   discard_on ActiveRecord::RecordNotFound
   discard_on ActiveJob::DeserializationError
 
+  after_perform :cleanup_job_contexts
+
   def set_error_context(**args)
     Rails.error.set_context(**args.transform_values(&:as_json))
+  end
+
+  def cleanup_job_contexts
+    JobContext.where(active_job_id: job_id).destroy_all
   end
 end
