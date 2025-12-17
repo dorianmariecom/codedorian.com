550dbdea9d4b29bfed45353b4b6f8ae2fb25304d
Dorian Marié <dorian@dorianmarie.com>
2025-12-17 17:11:08 +0000

    update datadog initializer for consistency with string quotes and line breaks

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 if Rails.env.production?
-  ENV["DD_GIT_COMMIT_SHA"] = ENV.fetch("KAMAL_VERSION") { `git rev-parse HEAD`.strip }
+  ENV["DD_GIT_COMMIT_SHA"] = ENV.fetch("KAMAL_VERSION") do
+    `git rev-parse HEAD`.strip
+  end
 
-  require 'datadog/auto_instrument'
+  require "datadog/auto_instrument"
 
   Datadog.configure do |c|
     c.env = ENV.fetch("CODE_ENV", "localhost")
-    c.service = 'codedorian.com'
+    c.service = "codedorian.com"
     c.tracing.sampling.default_rate = 1.0
     c.profiling.enabled = true
     c.appsec.enabled = true
