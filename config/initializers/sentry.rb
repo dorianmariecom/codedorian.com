d530934b09132c9d1695de8cd8db0e392ba27127
Dorian Marié <dorian@dorianmarie.com>
2025-12-25 20:41:34 +0100

    reduce sentry usage

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,12 +4,12 @@ unless ENV["DUMMY"]
   Sentry.init do |config|
     config.breadcrumbs_logger = %i[active_support_logger http_logger]
     config.dsn = Config.sentry.dsn.ruby
-    config.enable_logs = true
+    config.enable_logs = false
     config.enabled_patches = [:logger]
     config.environment = ENV.fetch("CODE_ENV", "localhost")
-    config.profiles_sample_rate = 1.0
+    config.profiles_sample_rate = 0
     config.release = ENV.fetch("KAMAL_VERSION", "unknown")
     config.send_default_pii = true
-    config.traces_sample_rate = 1.0
+    config.traces_sample_rate = 0
   end
 end
