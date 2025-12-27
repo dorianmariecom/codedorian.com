c43e23b530a2a4928ea45f107df6903bad9c3830
Dorian Marié <dorian@dorianmarie.com>
2025-12-27 18:07:49 +0100

    remove sentry feedback (was not working)

--- app/javascript/controllers/feedback_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-
-export default class extends Controller {
-  connect() {
-    const client = window.Sentry.getClient();
-    const feedback = client.getIntegrationByName("Feedback");
-    feedback.attachTo(this.element);
-  }
-}
