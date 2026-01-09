ca855eb0e23dc74533640ec5612dcf80ee1fd47d
Dorian Marié <dorian@dorianmarie.com>
2026-01-09 20:41:34 +0100

    remove worker timeout for dev

--- config/puma.rb
+++ config/puma.rb
@@ -4,21 +4,14 @@ require "concurrent-ruby"
 
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
-
 processor_count = Concurrent.physical_processor_count
 worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { processor_count })
-workers worker_count if worker_count > 1
-
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
-
 port = ENV.fetch("PORT", 3000)
 
-bind "tcp://0.0.0.0:#{port}"
-bind "tcp://[::]:#{port}"
-
-environment ENV.fetch("RAILS_ENV", "development")
-
-pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid")
-
-plugin :tmp_restart
+threads(min_threads_count, max_threads_count)
+workers(worker_count) if worker_count > 1
+bind("tcp://0.0.0.0:#{port}")
+bind("tcp://[::]:#{port}")
+environment(ENV.fetch("RAILS_ENV", "development"))
+pidfile(ENV.fetch("PIDFILE", "tmp/pids/server.pid"))
+plugin(:tmp_restart)
