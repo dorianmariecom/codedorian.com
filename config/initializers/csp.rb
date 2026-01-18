1405835bbba85bf772acdd6b83e1899b3ed7c73e
Dorian Marié <dorian@dorianmarie.com>
2026-01-18 09:35:57 +0100

    fix csp

--- config/initializers/csp.rb
+++ config/initializers/csp.rb
@@ -3,22 +3,36 @@
 Rails.application.config.content_security_policy do |policy|
   policy.default_src(
     :self,
-    "blob:",
-    "'unsafe-inline'",
+    :blob,
+    :data,
+    :unsafe_inline,
+
+    # Google / Maps
     "https://www.google.com",
     "https://www.gstatic.com",
     "https://maps.googleapis.com",
-    "https://fonts.googleapis.com",
-    "https://maps.gstatic.com",
-    "https://fonts.gstatic.com",
     "https://places.googleapis.com",
+    "https://maps.gstatic.com",
+
+    # Fonts
+    "https://fonts.googleapis.com",
+    "https://fonts.gstatic.com",
+
+    # Proxy
     "https://proxy.dorianmarie.com",
+
+    # Sentry
     "https://js-de.sentry-cdn.com",
     "https://browser.sentry-cdn.com",
     "https://*.ingest.de.sentry.io",
+
+    # HubSpot – scripts, analytics, banners, forms, APIs
     "https://*.hs-scripts.com",
     "https://*.hubspot.com",
-    "https://*.hubapi.com"
+    "https://*.hubapi.com",
+    "https://js-eu1.hs-banner.com",
+    "https://js-eu1.hs-analytics.net",
+    "https://js-eu1.hscollectedforms.net"
   )
 end
 
