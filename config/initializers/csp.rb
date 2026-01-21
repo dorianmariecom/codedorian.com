99cffed899473f21e7eb307bdb2e246c1f693479
Dorian Marié <dorian@dorianmarie.com>
2026-01-21 08:24:41 +0100

    fix csp for captcha google.com

--- config/initializers/csp.rb
+++ config/initializers/csp.rb
@@ -1,27 +1,25 @@
 # frozen_string_literal: true
 
 Rails.application.config.content_security_policy do |policy|
-  policy.default_src(
-    :self,
-    :blob,
-    :data,
-    :unsafe_inline,
-    # Google / Maps
+  google_sources = [
     "https://www.google.com",
     "https://www.gstatic.com",
+    "https://www.recaptcha.net"
+  ]
+
+  google_maps_sources = [
     "https://maps.googleapis.com",
     "https://places.googleapis.com",
-    "https://maps.gstatic.com",
-    # Fonts
-    "https://fonts.googleapis.com",
-    "https://fonts.gstatic.com",
-    # Proxy
-    "https://proxy.dorianmarie.com",
-    # Sentry
+    "https://maps.gstatic.com"
+  ]
+
+  sentry_sources = [
     "https://js-de.sentry-cdn.com",
     "https://browser.sentry-cdn.com",
-    "https://*.ingest.de.sentry.io",
-    # HubSpot – scripts, analytics, banners, forms, APIs
+    "https://*.ingest.de.sentry.io"
+  ]
+
+  hubspot_sources = [
     "https://*.hs-analytics.net",
     "https://*.hs-banner.com",
     "https://*.hs-scripts.com",
@@ -29,7 +27,22 @@ Rails.application.config.content_security_policy do |policy|
     "https://*.hsforms.com",
     "https://*.hubapi.com",
     "https://*.hubspot.com"
+  ]
+
+  policy.default_src(
+    :self,
+    :blob,
+    :data,
+    :unsafe_inline,
+    "https://fonts.googleapis.com",
+    "https://fonts.gstatic.com",
+    "https://proxy.dorianmarie.com",
+    *google_maps_sources,
+    *google_sources,
+    *hubspot_sources,
+    *sentry_sources
   )
+
 end
 
 Rails.application.config.content_security_policy_report_only = false
