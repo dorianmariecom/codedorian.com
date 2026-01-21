b28245e95475c6e097377832dc602645ce3693fe
Dorian Marié <dorian@dorianmarie.com>
2026-01-21 08:25:02 +0100

    format

--- config/initializers/csp.rb
+++ config/initializers/csp.rb
@@ -1,32 +1,32 @@
 # frozen_string_literal: true
 
 Rails.application.config.content_security_policy do |policy|
-  google_sources = [
-    "https://www.google.com",
-    "https://www.gstatic.com",
-    "https://www.recaptcha.net"
+  google_sources = %w[
+    https://www.google.com
+    https://www.gstatic.com
+    https://www.recaptcha.net
   ]
 
-  google_maps_sources = [
-    "https://maps.googleapis.com",
-    "https://places.googleapis.com",
-    "https://maps.gstatic.com"
+  google_maps_sources = %w[
+    https://maps.googleapis.com
+    https://places.googleapis.com
+    https://maps.gstatic.com
   ]
 
-  sentry_sources = [
-    "https://js-de.sentry-cdn.com",
-    "https://browser.sentry-cdn.com",
-    "https://*.ingest.de.sentry.io"
+  sentry_sources = %w[
+    https://js-de.sentry-cdn.com
+    https://browser.sentry-cdn.com
+    https://*.ingest.de.sentry.io
   ]
 
-  hubspot_sources = [
-    "https://*.hs-analytics.net",
-    "https://*.hs-banner.com",
-    "https://*.hs-scripts.com",
-    "https://*.hscollectedforms.net",
-    "https://*.hsforms.com",
-    "https://*.hubapi.com",
-    "https://*.hubspot.com"
+  hubspot_sources = %w[
+    https://*.hs-analytics.net
+    https://*.hs-banner.com
+    https://*.hs-scripts.com
+    https://*.hscollectedforms.net
+    https://*.hsforms.com
+    https://*.hubapi.com
+    https://*.hubspot.com
   ]
 
   policy.default_src(
@@ -42,7 +42,6 @@ Rails.application.config.content_security_policy do |policy|
     *hubspot_sources,
     *sentry_sources
   )
-
 end
 
 Rails.application.config.content_security_policy_report_only = false
