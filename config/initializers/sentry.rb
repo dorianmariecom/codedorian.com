d9b49748a18d18125151419422557f0f8a9dc2b6
Dorian Marié <dorian@dorianmarie.com>
2026-09-05 14:25:58 +0200

    fix sentry 7 configuration

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,8 +4,7 @@ if !ENV["DUMMY"] && (Rails.env.production? || Rails.env.staging?)
   Sentry.init do |config|
     config.breadcrumbs_logger = %i[active_support_logger http_logger]
     config.dsn = Config.sentry.dsn.ruby
-    config.enable_logs = false
-    config.enabled_patches = [:logger]
+    config.enabled_patches = []
     config.environment = ENV.fetch("CODE_ENV", "localhost")
     config.profiles_sample_rate = 0
     config.release = ENV.fetch("KAMAL_VERSION", "unknown")
